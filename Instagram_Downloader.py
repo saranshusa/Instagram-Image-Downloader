4e90a5049e067efea2f3a259f90b7c17a68ba7a5
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-05-08 04:10:32 +0530

    Update Instagram_Downloader.py

--- Instagram_Downloader.py
+++ Instagram_Downloader.py
@@ -7,14 +7,14 @@ install instaloader
 
 import instaloader
 
-def main:
+def main():
 
     print('INSTAGRAM POSTS AND PROFILE DOWNLOADER\n')
     username = input('Enter the username : ')
 
     L = instaloader.Instaloader()
 
-    option = int(input('What do you want to download ? \n1. Only Profile Picture\n2. All Post\'s Images and Videos\nEnter 1 or 2: '))
+    option = int(input('\nWhat do you want to download ? \n1. Only Profile Picture\n2. All Post\'s Images and Videos\nEnter 1 or 2: '))
     if option == 1:
         L.download_profile(username, profile_pic_only=True)
     else:
@@ -23,3 +23,6 @@ def main:
             L.download_post(post, username)
 
     print('A folder containing the downloaded resources will be created in the current directory if no errors were occured.')
+
+if __name__ == '__main__':
+    main()
